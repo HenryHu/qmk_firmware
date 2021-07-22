34d83282ec4b7556370357b00c36d796a2b2b6ce
Henry Hu <henry.hu.sh@gmail.com>
2021-07-21 22:56:05 -0400

    replace rgb keys with custom code

--- keyboards/keebio/kbo5000/keymaps/henryhu/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/henryhu/keymap.c
@@ -51,12 +51,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_all(
-    RESET,               RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,          _______, _______,          _______, NK_TOGG, KC_MSEL, KC_EJCT, KC_SLEP, KC_PWR,  KC_WAKE,
+    RESET,               _______, _______, _______, _______, _______, _______,          _______, _______,          _______, NK_TOGG, KC_MSEL, KC_EJCT, KC_SLEP, KC_PWR,  KC_WAKE,
     RGB_TOG,    KC_PAST, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6,          KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_PMNS, KC_PPLS, _______, _______, KC_MPLY, KC_MPRV,
-    RGB_MOD,    KC_CAPS, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, _______,                   _______, _______, _______, _______, KC_PSCR, KC_BRID, KC_BRIU, KC_NLCK, KC_MRWD, KC_MFFD,
+    _______,    KC_CAPS, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, _______,                   _______, _______, _______, _______, KC_PSCR, KC_BRID, KC_BRIU, KC_NLCK, KC_MRWD, KC_MFFD,
     MACRO3,     _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4, KC_BTN5,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLCK, KC_WSCH, _______, KC_PENT, KC_MSTP, KC_MNXT,
     MACRO4,    CMD_MODE, _______, KC_WH_U, KC_WH_D, KC_WH_L, KC_WH_R, _______,          KC_CALC, KC_MAIL, KC_WBAK, KC_WFWD, KC_PSLS,         _______,          _______,
-    SET_TIMER,  _______, _______, KC_RWIN, KC_RALT, _______, _______,                   MO(1),   _______, KC_APP,  _______,                   _______, _______, _______, _______
+    SET_TIMER,  _______, _______, KC_RWIN, KC_RALT, MO(3),   MO(3),                     MO(1),   _______, KC_APP,  _______,                   _______, _______, _______, _______
   ),
 
   /*
@@ -134,6 +134,9 @@ bool process_key_down(uint16_t keycode, keyrecord_t *record) {
             register_code(KC_TAB);
             break;
 #endif
+        case RGB_TOG:
+            rgblight_toggle_noeeprom();
+            break;
 #ifdef ENABLE_CMDMODE
         default:
             if (command_mode) {
@@ -518,13 +521,21 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else */
     if (index == RIGHT_HALF_ENC1) {
-        if (IS_LAYER_ON(1)) {
+        if (IS_LAYER_ON(3)) {
             if (clockwise) {
-                rgblight_increase_hue();
+                rgblight_increase_hue_noeeprom();
                 setInfoLine("Hue UP");
             } else {
-                rgblight_decrease_hue();
+                rgblight_decrease_hue_noeeprom();
                 setInfoLine("Hue DOWN");
+            }
+        } else if (IS_LAYER_ON(1)) {
+            if (clockwise) {
+                rgblight_increase_sat_noeeprom();
+                setInfoLine("Sat UP");
+            } else {
+                rgblight_decrease_sat_noeeprom();
+                setInfoLine("Sat DOWN");
             }
         } else if (IS_LAYER_ON(2)) {
             if (clockwise) {
@@ -544,7 +555,15 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             }
         }
     } else if (index == RIGHT_HALF_ENC2) {
-        if (IS_LAYER_ON(1)) {
+        if (IS_LAYER_ON(3)) {
+            if (clockwise) {
+                rgblight_increase_val_noeeprom();
+                setInfoLine("Val UP");
+            } else {
+                rgblight_decrease_val_noeeprom();
+                setInfoLine("Val DOWN");
+            }
+        } else if (IS_LAYER_ON(1)) {
             if (clockwise) {
                 rgblight_step();
                 setInfoLine("RGB NEXT");
