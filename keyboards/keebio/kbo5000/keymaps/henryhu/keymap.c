e1bab827ddf1919390081a06c950c638cc132d52
Henry Hu <henry.hu.sh@gmail.com>
2022-06-28 22:01:52 -0400

    for new keyboard

--- keyboards/keebio/kbo5000/keymaps/henryhu/keymap.c
+++ keyboards/keebio/kbo5000/keymaps/henryhu/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     S(C(KC_T)), KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, KC_END,
     MACRO1,     MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_DEL,  KC_PGDN,
     MACRO2,     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENTER),          KC_UP,
-    ALT_TAB,    KC_LCTL, MO(2),   KC_LGUI, KC_LALT, KC_SPC,  KC_SPC,                    MO(1),   KC_SPC,  MO(2),   LT(1, KC_RGUI),            KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ALT_TAB,    KC_LCTL, MO(2),   KC_LGUI, KC_LALT, KC_LALT, KC_SPC,                    KC_SPC,  KC_SPC,  KC_RALT, LT(1, KC_RGUI),            KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [1] = LAYOUT_all(
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,    KC_CAPS, KC_BTN1, KC_MS_U, KC_BTN2, KC_BTN3, _______,                   _______, _______, _______, _______, KC_PSCR, KC_BRID, KC_BRIU, KC_NLCK, KC_MRWD, KC_MFFD,
     MACRO3,     _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN4, KC_BTN5,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SLCK, KC_WSCH, _______, KC_PENT, KC_MSTP, KC_MNXT,
     MACRO4,    CMD_MODE, _______, KC_WH_U, KC_WH_D, KC_WH_L, KC_WH_R, _______,          KC_CALC, KC_MAIL, KC_WBAK, KC_WFWD, KC_PSLS,         _______,          _______,
-    SET_ALARM,  _______, MO(3),   KC_RWIN, KC_RALT, MO(3),   MO(3),                     MO(1),   _______, KC_APP,  _______,                   _______, _______, _______, _______
+    SET_ALARM,  _______, MO(3),   KC_RWIN, KC_RALT, MO(2),   MO(2),                     MO(1),   _______, KC_APP,  _______,                   _______, _______, _______, _______
   ),
 
   /*
