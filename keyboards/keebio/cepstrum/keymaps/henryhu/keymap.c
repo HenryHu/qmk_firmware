511945580e327f0b83b21203b09c78aef997eebe
Henry Hu <henry.hu.sh@gmail.com>
2025-02-09 17:27:30 -0500

    update keymap

--- keyboards/keebio/cepstrum/keymaps/henryhu/keymap.c
+++ keyboards/keebio/cepstrum/keymaps/henryhu/keymap.c
@@ -9,16 +9,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_65xt(
-    KC_F1,   KC_GRV,  QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+    KC_F1,   KC_F2,   QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
     KC_F3,   KC_F4,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-    MACRO1,  MACRO2,  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+    MACRO1,  MACRO2,  CTL_T(KC_GRV),KC_A,   KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
     KC_VOLD, KC_VOLU, KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,             KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
     KC_MPLY, KC_INS,  KC_LCTL, KC_LGUI, MO(1),   KC_LALT, KC_SPC,                             KC_SPC,  KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_65xt(
     RM_HUEU, RM_HUED, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_INS,
     RM_SATU, RM_SATD, RM_TOGG, RM_NEXT, KC_UP,   _______, QK_BOOT, EE_CLR,           _______, _______, _______, _______, KC_SYRQ, KC_BRK,  KC_NUM,  KC_SCRL, KC_END,
-    RM_VALU, RM_VALD, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_WFWD,          KC_WBAK, _______, _______, _______, _______,          _______, MS_BTN2, KC_MPRV,
+    RM_VALU, RM_VALD, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_WFWD,          KC_WBAK, _______, _______, _______, _______,          _______, MS_BTN2, KC_MPRV,
     _______, _______, _______,          _______, _______, KC_CALC, _______, KC_WHOM,          _______, EE_CLR,  _______, _______, _______, MS_BTN1, MS_UP,   KC_MNXT,
     _______, _______, _______, _______, MO(2),   _______, _______,                            _______, KC_RGUI, MO(2), KC_APP,  MS_LEFT, MS_DOWN, MS_RGHT
   )
